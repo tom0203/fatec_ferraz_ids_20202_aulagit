23874055396effb09a8a4dc1e8bbd207c2cf5677
Everton Santos <everton.silva.s@hotmail.com>
2020-09-08 10:50:02 -0300

    Jaz faz soma e subtracao

--- aula.py
+++ aula.py
@@ -1,5 +1,10 @@
+print ("Soma ou Subtração")
+op = int  (input("1-Soma ou 2-Subtração"))
 a = int (input("Digite o primeiro valor: "))
 b = int (input("Digite o segundo valor: "))
-resultado = a+b
+	if op == 1:
+	resultado = a+b
+	else
+	resultado = a-b
 print (resultado)
 
